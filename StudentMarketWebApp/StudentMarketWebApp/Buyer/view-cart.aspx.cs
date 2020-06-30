f7ab2028ccacf47bad3fd51d6fb960efa538dfca
shfsakib <40665494+shfsakib@users.noreply.github.com>
2020-06-30 20:24:47 +0600

    Update view-cart.aspx.cs

--- StudentMarketWebApp/StudentMarketWebApp/Buyer/view-cart.aspx.cs
+++ StudentMarketWebApp/StudentMarketWebApp/Buyer/view-cart.aspx.cs
@@ -4,11 +4,30 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BitsSoftware;
+using StudentMarketWebApp.DAL.Gateway;
+using StudentMarketWebApp.DAL.Model;
 
 namespace StudentMarketWebApp.Buyer
 {
     public partial class view_cart : System.Web.UI.Page
     {
+        private BaseClass func;
+        private Alert alert;
+        private PostAdModel postAdModel;
+        private PostAdGateway postAdGateway;
+        private BuyModel buyModel;
+        private BuyGateway buyGateway;
+
+        public view_cart()
+        {
+            func = BaseClass.GetInstance();
+            alert = Alert.GetInstance();
+            postAdModel = PostAdModel.GetInstance();
+            postAdGateway = PostAdGateway.GetInstance();
+            buyModel = BuyModel.GetInstance();
+            buyGateway = BuyGateway.GetInstance();
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -16,7 +35,17 @@ namespace StudentMarketWebApp.Buyer
 
         protected void logOut_OnServerClick(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            func.Logout();
+        }
+
+        protected void cartGridView_OnPageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            
+        }
+
+        protected void cartGridView_OnRowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            
         }
     }
 }
