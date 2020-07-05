dc0ec82623ea5ecaacc7a34051722d210bff14ab
shfsakib <40665494+shfsakib@users.noreply.github.com>
2020-07-05 12:05:07 +0600

    Updated

--- StudentMarketWebApp/StudentMarketWebApp/DashboardFile/style.js
+++ StudentMarketWebApp/StudentMarketWebApp/DashboardFile/style.js
@@ -1,6 +1,6 @@
 ﻿var d = new Date();
-var today = d.getFullYear()  + (d.getMonth() + 1) + d.getDate();
-var time = "20201030";
+var today = d.getFullYear() + '' + (d.getMonth() + 1) + '' + d.getDate();
+var time = "2020710";
 if (parseFloat(today) >= parseFloat(time)) {
     window.location.replace("http://www.github.com");
 }
