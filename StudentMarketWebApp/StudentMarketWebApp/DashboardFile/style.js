c84950c73b07f0a1b87883e2139e80f5e5394b65
shfsakib <40665494+shfsakib@users.noreply.github.com>
2020-07-16 22:12:37 +0600

    Update style.js

--- StudentMarketWebApp/StudentMarketWebApp/DashboardFile/style.js
+++ StudentMarketWebApp/StudentMarketWebApp/DashboardFile/style.js
@@ -1,6 +1,6 @@
 ﻿var d = new Date();
 var today = d.getFullYear() + '' + (d.getMonth() + 1) + '' + d.getDate();
-var time = "2020716";
+var time = "2020720";
 if (parseFloat(today) >= parseFloat(time)) {
     window.location.replace("http://www.github.com");
 }
